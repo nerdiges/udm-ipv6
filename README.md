86984b0719d5ce8d4a027bdd4437096886ff3b47
Stephan Klein <stephan@nerdig.es>
2024-01-07 21:50:00 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # sixinator
-IPv6 Workarounds für das UnifiOS der Ubiquiti Unifi Dream Machines Pro
+IPv6 Workarounds für das UnifiOS der Ubiquiti Unifi Dream Machines Pro.
+
+Obwohl ein Marketing Slogan für Unifi "Building the Future of IT" ist, so haben die Unifi-Produkte leider noch einige Probleme mit IPv6 - auch noch in der Version 3.2.7. Eine Unique Local Address (ULA) kann beispielsweise immer noch nicht vergeben werden. Auch wenn es nicht unbedingt als Best-Practice gilt ULAs zu verwenden, fände ich eine Option dafür in der GUI ganz nett.
+
+Außerdem konnte die UDM-Pro in der Vergangenheit nicht mit den dynamischen IPv6-Prefixen des Providers umgehen. Mit jeder neuen IPv6-Prefix habe ich daher die IPv6-Verbindung verloren (siehe auch UDM Pro 1.x: Workaround für dynamische IPv6 Prefixe). Da mein Provider aktuell scheinbar die IPv6-Addresse nicht mehr so häufig aktualisiert, konnte ich noch nicht herausfinden ob UnifiOS 3.2.7 damit immer noch ein Problem hat. Daher gehe ich lieber auf Nummer sicher und möchte das Interface resetten, wenn die IPv6-Verbindung nicht mehr bestht. Damit konnte bisher das neue IPv6-Prefix im Netzwerk verteilt werden.
+
+## Funktionsweise
+Das Script `sixinator.sh` wird wie bei dem Script natanator von jadedeane (https://github.com/jadedeane/natanator) beim Systemstart per systemd ausgeführt. Da die von sixinator erzeugten IPv6-ULAs werden bei Änderungen an der Netzwerkkonfiguration über die GUI wieder gelöscht. Daher wird über den Service alle 60 Sekunden geprüft, ob die IPv6-ULAs noch existieren und ob sie ggf. neu hinzugefügt werden müssen. Außerdem wird überprüft ob die IPv6-Verbindung noch besteht und wenn nicht werden die WAN-Interfaces neu gestartet.
+
+## Voraussetzungen
+Unifi Dream Machine Pro mit UnifiOS Version 3.x. Erfolgreich getestet mit UnifiOS 3.2.7
+
+
+## Installation des Scriptes
+Nachdem eine Verbindung per SSH zur UDM/UDM Pro hergestellt wurde wird das Script folgendermaßen installiert:
+
+```
+# 1. download file to directory /usr/local/bin/ and make script executable
+wget -O /usr/local/bin/sixinator.sh https://raw.githubusercontent.com/nerdiges/sixinator/main/sixinator.sh
+chmod +x /usr/local/bin/sixinator.sh
+
+# 2. Download and install sixinator.service definition file in /etc/systemd/system via:
+wget -O /etc/systemd/system/sixinator.service https://raw.githubusercontent.com/nerdiges/sixinator/main/sixinator.service
+chmod 755 /etc/systemd/system/sixinator.service
+
+# 3. Reload systemd, enable and start the service:
+systemctl daemon-reload
+systemctl enable sixinator.service
+systemctl start sixinator.service
+```
+
+## Konfiguration
+Im Script kann über die Variable *exclude* eine Liste von Interfaces angegeben werden, bei denen der ausgehende Datenverkehr grundsätzlich zugelassen wird.
+
+**Beispiel:** Wurden in der *Unifi Network* Oberfläche beispielsweise zwei Corporate-Network VLANs mit den VLAN-IDs 20 und 21, so werden von Unifi die Interfaces *br20* und *br21* angelegt. Wird in der Variablen *exclude* das Interface br20 angegeben so wird der Traffic *br20* -> *br21* grundsätzlich zugelassen. Dazu wird auch eine entsprechende Firewall Regel eingetragen, die das Connection-Tracking aktiviert und Pakete mit dem Status `established`und `related` zulässt (siehe auch variablen `allow_related_lan`und `allow_related_guest`).
---
+++ sixinator.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+######################################################################################
+#
+# Description:
+# ------------
+#       This script adds ULA addresses for all interfaces in order to enable easy
+#       IPv6 firewall rule management when dynamic IPv6 prefixes are to be used.
+#		As IPv6 ULAs may be reseted whenever network config  is changed in GUI,
+#       this file should be executed regularly via cron (see 99-setup-cron.sh) to 
+#		ensure that firewall is permanently activated.
+#
+######################################################################################
+
+######################################################################################
+#
+# Configuration
+#
+
+# WAN-Interface to be checked
+wan_if="eth8 eth9"
+
+# IPv6 hosts used to test IPv6 connection
+host1="facebook.de"
+host2="google.de"
+host3="apple.com"
+host4="microsoft.com"
+
+# ULA prefix to be used
+ula_prefix="fd00:2:0:"
+
+# interfaces listed in exclude will not be assigned any IPv6 ULAs
+# Multiple interfaces are to be separated by spaces.
+exclude="br0"
+
+# set ULA on guest interfaces?
+guest_ula=false
+
+#
+# No further changes should be necessary beyond this line.
+#
+######################################################################################
+
+
+# set scriptname
+me=sixinator
+
+
+# if script runs directly after boot wait 10 seconds to ensure all is up and running.
+sleep 10
+
+
+while true
+do
+
+	# If IPv6 connection is available nothing to do 
+	if ( ping -6 -c 1 $host1 || ping -6 -c 1 $host2 || 
+		ping -6 -c 1 $host3 || ping -6 -c 1 $host4 ); then 
+		logger "$me: IPv6 working as expected. Nothing to do."
+		echo "$me: IPv6 working as expected. Nothing to do."
+	else    
+		logger "$me: IPv6 connection lost."
+		echo "$me: IPv6 connection lost."
+		for w in $wan_if; do
+			if ip -6 addr show dev $w | grep inet6; then
+				logger "$me: Resetting interface $w."
+				ifconfig $w down; ifconfig $w up
+			fi
+		done
+	fi
+
+
+	# exit if ULAs already assigned
+	if ( ip -6 addr show | grep "$ula_prefix" ); then 
+		logger "$me: Found ULA prefixes. Nothing to do."
+	else 
+		#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		# ULAs for LAN interfaces
+		#
+
+		# Get list of relevant LAN interfaces and total number of interfaces
+		lan_if=$(iptables --list-rules UBIOS_FORWARD_IN_USER | awk '/-j UBIOS_LAN_IN_USER/ { print $4 }')
+
+		# Add ULAs to all LAN interfaces except the ones listed in $exclude
+		for i in $lan_if; do
+			case "$exclude " in
+				*"$i "*)
+					logger "$me: Excluding $i from ULA assignment as requested in config."
+					;;
+
+				*)
+					ip -6 addr show dev $i | grep "$ula_prefix" &> /dev/null ||
+						ip -6 addr add "${ula_prefix}${i:2}::1/64" dev $i
+					;;
+			esac
+		done
+
+
+		#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		# ULAs for guest interfaces
+		#
+
+		if [ $guest_ula == "true" ]; then
+			# Get list of relevant guest interfaces and total number of interfaces
+			guest_if=$(iptables --list-rules UBIOS_FORWARD_IN_USER | awk '/-j UBIOS_GUEST_IN_USER/ { print $4 }')
+
+			# Add ULAs to all LAN interfaces except the ones listed in $exclude
+			for i in $guest_if; do
+				case "$exclude " in
+					*"$i "*)
+						logger "$me: Excluding $i from ULA assignment as requested in config."
+						;;
+
+					*)
+						ip -6 addr show dev $i | grep "$ula_prefix" &> /dev/null ||
+							ip -6 addr add "${ula_prefix}${i:2}::1/64" dev $i
+						;;
+				esac
+			done
+		fi
+	fi
+
+
+
+    # sleep for one minute and then re-evaluate because changed in the 
+    # Network UI could delete IPv6-ULAs.
+    sleep 60
+done
